2afe645a062f7b90c155d7c199ed56a4400a534c
UnesseAh <youness.ahasla@gmail.com>
2026-06-25 22:08:53 +0100

    create drawer header and style it and with colors and an image

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: HomePage(),
       theme: ThemeData(
+        primaryColor: Colors.teal,
         appBarTheme: AppBarTheme(
           backgroundColor: Colors.deepOrange,
           foregroundColor: Colors.white,
@@ -31,6 +32,32 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.white, Theme.of(context).primaryColor],
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage("images/youness.png"),
+                  ),
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage("images/youness.png"),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(title: Text("Home Page"), centerTitle: true),
       body: Center(
         child: Text(
