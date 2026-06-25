57fb6f2661749d76c4d45b242bf81231dddf466c
UnesseAh <youness.ahasla@gmail.com>
2026-06-25 22:21:31 +0100

    fix: import the widget drawer in HomePage

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_dwm/widgets/drawer.widget.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: MyDrawer()
-      ),
+      drawer: MyDrawer(),
       appBar: AppBar(title: Text("Home Page"), centerTitle: true),
       body: Center(
         child: Text(
